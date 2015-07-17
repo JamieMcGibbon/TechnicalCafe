743be7d861f8a7d03869803e0a24828ceded3b54
Jamie <JamieMcGibbon@users.noreply.github.com>
2015-07-17 00:47:21 -0400

    Create Java Tutorial 10 - Do While Loops.java

---
+++ Java-Tutorials/Java Tutorial 10 - Do While Loops.java
@@ -0,0 +1,19 @@
+/* YouTube Link: https://www.youtube.com/watch?v=U38mE9aIhHY */
+
+public class DoWhileLoops {
+
+	public static void main(String[] args) {
+		
+		int x = 1000;
+		
+		do{
+			//body of do-while loop
+			System.out.println(x);
+			x++;
+			
+		}
+		while(x <= 10);
+
+	}
+
+}
