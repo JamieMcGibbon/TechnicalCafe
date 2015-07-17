224726976a21feeaa76733e458e783ddd9c3e160
Jamie <JamieMcGibbon@users.noreply.github.com>
2015-07-17 00:51:22 -0400

    Create Java Tutorial 12 - Getting User Input.java

---
+++ Java-Tutorials/Java Tutorial 12 - Getting User Input.java
@@ -0,0 +1,21 @@
+/* YouTube Link: https://www.youtube.com/watch?v=1n8KU0nVjmc */ 
+
+import java.util.Scanner;
+
+public class UserInput {
+
+	public static void main(String [] args){
+
+		Scanner input = new Scanner(System.in);
+		
+		int num;
+		
+		System.out.println("Please enter a number");
+		
+		num = input.nextInt();
+		
+		System.out.println(num);
+
+	}
+
+}
