ed351f523b3356f1bb4bbca5fc19fe22fc9459fb
Jamie <JamieMcGibbon@users.noreply.github.com>
2015-07-17 00:46:32 -0400

    Create Java Tutorial 9 - While Loops.java

---
+++ Java-Tutorials/Java Tutorial 9 - While Loops.java
@@ -0,0 +1,18 @@
+/* YouTube Link: https://www.youtube.com/watch?v=fDz1zbwhxvY */
+
+public class WhileLoops {
+
+	public static void main(String[] args) {
+		
+		int i = 1;
+		
+		while(i <= 10){
+			
+			System.out.println(i);
+			i++;
+			
+		}
+
+	}
+
+}
