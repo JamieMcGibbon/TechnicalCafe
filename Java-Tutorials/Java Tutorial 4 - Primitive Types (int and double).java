d09c4fcd053a30a6b7c416144c6e4684a1261e20
Jamie <JamieMcGibbon@users.noreply.github.com>
2015-07-17 00:41:47 -0400

    Create Java Tutorial 4 - Primitive Types (int and double).java

---
+++ Java-Tutorials/Java Tutorial 4 - Primitive Types (int and double).java
@@ -0,0 +1,17 @@
+/* YouTube Link: https://www.youtube.com/watch?v=ST67iEwjilU */
+
+public class PrimitiveTypes {
+
+	public static void main(String[] args) {
+		
+		int intOne = 12;
+		
+		double doubleOne = 10.3;
+		
+		double doMath = doubleOne + 2.4;
+		
+		System.out.println(doMath);
+
+	}
+
+}
