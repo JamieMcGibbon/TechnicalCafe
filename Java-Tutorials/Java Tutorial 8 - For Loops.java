b51de59b0d1fedf4b5557fcaefcf83be4b68862f
Jamie <JamieMcGibbon@users.noreply.github.com>
2015-07-17 00:45:42 -0400

    Create Java Tutorial 8 - For Loops.java

---
+++ Java-Tutorials/Java Tutorial 8 - For Loops.java
@@ -0,0 +1,17 @@
+/* YouTube Link: https://www.youtube.com/watch?v=AyBdZ64XneI */
+
+public class ForLoops {
+
+	public static void main(String[] args) {
+
+		for(int i = 10; i >= 1; i--){
+			
+			System.out.println(i);
+			System.out.println("Hey");
+			
+		}
+		
+
+	}
+
+}
