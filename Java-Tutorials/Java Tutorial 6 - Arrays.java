fc14900252329db8186608f7c6a07422959499d6
Jamie <JamieMcGibbon@users.noreply.github.com>
2015-07-17 00:43:32 -0400

    Create Java Tutorial 6 - Arrays.java

---
+++ Java-Tutorials/Java Tutorial 6 - Arrays.java
@@ -0,0 +1,28 @@
+/* YouTube Link: https://www.youtube.com/watch?v=xxd7xrUIWyc */
+
+public class ArrayTutorial {
+
+	public static void main(String[] args) {
+		
+		int[] myArray = {1, 2, 3, 4, 5};
+		
+		System.out.println(myArray[3]);
+		
+		int[] myValues = new int[3];
+		
+		myValues[0] = 100;
+		
+		myValues[1] = 200;
+		
+		myValues[2] = 300;
+		
+		System.out.println(myValues[1]);
+		
+		int myInt = myArray[3] + myValues[2];
+		
+		System.out.println(myInt);
+		
+
+	}
+
+}
