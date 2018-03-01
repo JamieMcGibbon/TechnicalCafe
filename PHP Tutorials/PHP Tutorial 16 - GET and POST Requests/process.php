6f1e6194e23c911e85ad590099a558eab30f9ed1
Jamie McGibbon <JamieMcGibbon@users.noreply.github.com>
2018-03-01 13:40:41 -0500

    Added code for PHP Tutorial 16
    Added code for PHP Tutorial 16 (GET and POST requests)

---
+++ PHP Tutorials/PHP Tutorial 16 - GET and POST Requests/process.php
@@ -0,0 +1,25 @@
+<?php
+
+  //Picking up data using GET requests
+  // $username = $_GET['username'];
+  // $password = $_GET['password'];
+  // $submit = $_GET['submit'];
+  //
+  // echo $username;
+  // echo "<br />";
+  // echo $password;
+  // echo "<br />";
+  // echo $submit;
+
+  //Picking up data using POST requests
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  $submit = $_POST['submit'];
+
+  echo $username;
+  echo "<br />";
+  echo $password;
+  echo "<br />";
+  echo $submit;
+
+?>
