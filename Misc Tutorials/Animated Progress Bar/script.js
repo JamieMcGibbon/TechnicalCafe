7478b12a8e38f553cbc2c40f7b3902c3d54ece05
Jamie McGibbon <JamieMcGibbon@users.noreply.github.com>
2018-05-27 19:32:10 -0400

    Refactored Animated Progress Bar source
    Simplified JS code for the Animated Progress Bar tutorial

--- Misc Tutorials/Animated Progress Bar/script.js
+++ Misc Tutorials/Animated Progress Bar/script.js
@@ -1,25 +1,22 @@
 // Start the progress bar with a width of 0%
-        let num = 0;
+        let fill = 0;
 
         // Create a function that will cause the bar to load at a speed of 50 ms
         function load_bar(){
-           // Call the "load()" function ever 50 ms
-           window.setInterval(load, 50);
-        }
 
-        // Function that actually loads the blue striped progress bar
-        function load(){
+           // Call the "load()" function every 50 ms
+           window.setInterval(function(){
             // Add 10% for each iteration of the function (should be 10 iterations in total)
-            num += 10;
-            // Create a variable that will be used to define the width of the progress bar as a percentage
-            let percent_loaded = num + "%";
+            fill += 10;
 
             // If the percentage of the progress bar that is loaded === 100, stop the iteration
-            if(num === 100){
+            if(fill === 100){
                 clearInterval();
             }
-            // Continue the iteration as long as the value of num < 100
+            // Continue the iteration as long as the value of fill < 100
             else{
-                document.getElementById("progress_one").style.width = percent_loaded;
+                document.getElementById("progress_one").style.width = fill + "%";
             }
+
+           }, 50);
         }
